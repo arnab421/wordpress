bf82afe87ea7e1889d5982144f5b2fbe5e68afe5
arnab421 <61282700+arnab421@users.noreply.github.com>
2024-09-30 01:20:48 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,190 @@
+<footer class="mt-0 bg--mbBlue">
+    <div class="footer-cta">
+        <a href="javascript:void(0)" class="c2a c2a-arw c2a--inline bg--mbOrange c--mbWhite hvr:bg--mbBlack hvr:c--mbWhite career-modal-link">
+            <span>Let's Talk</span>
+            <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#fff">
+                <path d="M745.85-450.39H129.62q-12.75 0-21.38-8.62-8.62-8.63-8.62-21.39 0-12.75 8.62-21.37 8.63-8.61 21.38-8.61h616.23L572.38-684.46q-8.17-8.21-8.55-20.53-.37-12.32 8.55-21.31 8.18-8.24 20.82-8.24 12.65 0 20.95 8.31l220.54 220.54q5.62 5.61 7.92 11.9 2.31 6.28 2.31 13.46 0 7.18-2.32 13.64-2.33 6.46-7.91 12L614.15-234.15q-8.21 8.3-20.53 8.3-12.31 0-21.24-8.3-9.3-8.93-9.11-21.39.19-12.46 9.11-21.38l173.47-173.47Z" />
+            </svg>
+        </a>
+    </div>
+    <div class="main-container">
+        <div class="inner-container">
+            <div class="copyright">
+                <p class="h6 c--mbWhite">© Marble Box – <script type="text/javascript">
+                        var year = new Date();
+                        document.write(year.getFullYear());
+                    </script>. All rights reserved.</p>
+                <ul class="footer-link">
+                    <li><a href="javascript:void(0)" class="terms-link">Privacy Policy</a></li>
+                </ul>
+            </div>
+            <div class="bsi-logo">
+                <figure><img src="<?php bloginfo('template_url'); ?>/assets/img/icons/bsi.webp" alt="BSI" title="BSI"></figure>
+                <article>
+                    <p class="h6 c--mbWhite">MARBLE BOX is <br />ISO27001:2013 Certified</p>
+                </article>
+            </div>
+        </div>
+    </div>
+    <figure class="footer-logo">
+        <img src="<?php bloginfo('template_url'); ?>/assets/img/logo-footer.webp" alt="Marblebox" title="Marblebox">
+    </figure>
+</footer>
+
+<!-- Career Modal -->
+<div class="career-modal-overlay"></div>
+<div class="career-modal bg--mbWhite">
+    <div class="modal-header">
+        <span class="c--mbBlack h5 fw--l close">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 13 13">
+                <polyline class="arrow" fill="none" stroke="currentColor" points="1 1,6.5 6.5,12 1" />
+                <polyline class="arrow" fill="none" stroke="currentColor" points="1 12,6.5 6.5,12 12" />
+            </svg>
+        </span>
+    </div>
+    <div class="modal-body" data-scrollbar>
+        <div class="hero-form flow-rootx">
+            <p class="h2 form-head fw--b c--mbBlack text-left">Get in Touch</p>
+            <?php echo do_shortcode('[contact_us_request form_type="special"]'); ?>
+
+        </div>
+    </div>
+</div>
+<!-- Career Modal -->
+
+<!-- privacy pop up -->
+<!-- Privacy Policy Popup -->
+<div class="privacy-modal-overlay"></div>
+<div class="privacy-modal bg--mbWhite">
+    <div class="modal-header">
+        <span class="c--mbBlack h5 fw--l close2" style="cursor: pointer;">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 13 13">
+                <polyline class="arrow" fill="none" stroke="currentColor" points="1 1,6.5 6.5,12 1"></polyline>
+                <polyline class="arrow" fill="none" stroke="currentColor" points="1 12,6.5 6.5,12 12"></polyline>
+            </svg>
+        </span>
+    </div>
+    <div class="modal-body" data-scrollbar>
+        <p class="h2 form-head fw--b c--mbBlack text-left">Privacy Policy</p>
+        <div class="flow-rootX2 mt-2">
+            <p class="h4 fw--sb">WHAT INFORMATION DO WE COLLECT?</p>
+            <p class="h5">We may collect, store and use the following kinds of personal data:</p>
+            <ul class="modl">
+                <li>Information about your visits to and use of this website;</li>
+                <li>Information about any transactions carried out between you and us on or in relation to this website;</li>
+                <li>Information that you provide to us for the purpose of registering with us and/or subscribing to our website services and/or email notifications.</li>
+            </ul>
+            <p class="h5">We may collect, store and use non-personal data about your visits to and use of this website.</p>
+            <p class="h4 fw--sb">INFORMATION ABOUT WEBSITE VISITS</p>
+            <p class="h5">We may collect information about your computer and your visits to this website such as your IP address, geographical location, browser type, referral source, length of visit and number of page views. We may use this information in the administration of this website, to improve the website’s usability, and for marketing purposes.</p>
+            <p class="h5">We use cookies on this website. A cookie is a text file sent by a web server to a web browser, and stored by the browser. The text file is then sent back to the server each time the browser requests a page from the server. This enables the web server to identify and track the web browser.</p>
+            <p class="h5">We may send a cookie which may be stored on by your browser on your computer’s hard drive.</p>
+            <p class="h5">We may use the information we obtain from the cookie in the administration of this website, to improve the website’s usability and for marketing purposes. We may also use that information to recognize your computer when you visit our website, and to personalize our website for you.</p>
+            <p class="h5">We may use anonymous cookies to record non-personal information such as website activity, date and time of visit, and domain type. We may use this information for retargeting purposes.</p>
+            <p class="h5">Most browsers allow you to refuse to accept cookies. (For example, in Internet Explorer you can refuse all cookies by clicking “Tools”, “Internet Options”, “Privacy”, and selecting “Block all cookies” using the sliding selector.) This will, however, have a negative impact upon the usability of many websites, including this one.</p>
+            <p class="h4 fw--sb">USING YOUR PERSONAL DATA</p>
+            <p class="h5">Personal data submitted on this website will be used for the purposes specified in this privacy policy or in relevant parts of the website.
+                In addition to the uses identified elsewhere in this privacy policy, we may use your personal information to:</p>
+            <ul class="modl">
+                <li>Improve your browsing experience by personalizing the website;</li>
+                <li>Send information (other than marketing communications) to you which we think may be of interest to you by post or by email or similar technology;</li>
+                <li>Send to you marketing communications relating to our business or the businesses of carefully-selected third parties which we think may be of interest to you by post or, where you have specifically agreed tothis, by email or similar technology.</li>
+            </ul>
+            <p class="h4 fw--sb">OTHER DISCLOSURES</p>
+            <p class="h5">In addition to the disclosures reasonably necessary for the purposes identified elsewhere in this privacy policy, we may disclose information about you:</p>
+            <ul class="modl">
+                <li>To the extent that we are required to do so by law;</li>
+                <li>In connection with any legal proceedings or prospective legal proceedings;</li>
+                <li>In order to establish, exercise or defend our legal rights (including providing information to others for the purposes of fraud prevention and reducing credit risk).</li>
+            </ul>
+            <p class="h4 fw--sb">SECURITY OF YOUR PERSONAL DATA</p>
+            <p class="h5">We will take reasonable precautions to prevent the loss, misuse or alteration of your personal information. Of course, data transmission over the Internet is inherently insecure, and we cannot guarantee the security of data sent over the Internet.</p>
+            <p class="h5">We may occasionally use other companies to set cookies on our Web Site and gather cookie information for us. In some cases we may also use another company to operate web servers for our Web Site. We use the cookie information gathered by these companies in the same manner as stated above.</p>
+            <p class="h4 fw--sb">POLICY AMENDMENTS</p>
+            <p class="h5">We may change this Statement at any time and from time to time. The most recent version of the Statement is reflected by the version date located at the bottom of this Statement. This Statement is not intended to and does not create any contractual or other legal right in or on behalf of any party. You should check this page occasionally to ensure you are happy with any changes.</p>
+            <p class="h4 fw--sb">CONTACT</p>
+            <p class="h5">If you have any questions about this privacy policy or our treatment of your personal data, please write to us by email to privacypolicy_mb@marblebox.com.</p>
+        </div>
+    </div>
+</div>
+<!-- privacy pop up -->
+<!-- add popup  -->
+<div class="pop-up-overlay" style="display:none;">
+    <div class="pop-up-msg">
+        <span>
+            <svg class="ico-svg checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+                <circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"></circle>
+                <path class="checkmark__check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8"></path>
+            </svg>
+            <svg class="ico-svg cross" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
+                <circle class="checkmark__circle" cx="26" cy="26" r="25" fill="none"></circle>
+                <path class="checkmark__check" fill="none" d="M16 16 36 36 M36 16 16 36"></path>
+            </svg>
+        </span>
+        <div class="content">
+            <h6></h6>
+            <p></p>
+        </div>
+    </div>
+</div>
+<!-- add popup -->
+
+<script src="<?php bloginfo('template_url'); ?>/assets/js/jquery-3.5.1.min.js?v=<?php echo rand() ?>"></script>
+<script>
+    jQuery(document).ready(function() {
+
+        
+        // For the first form
+        jQuery('#gt_phone').on('keyup change', function() {
+            // Get the instance of the intlTelInput initialized on this specific input field
+            var itiInstance = window.intlTelInputGlobals.getInstance(jQuery('#gt_phone')[0]);
+
+            if (itiInstance) {
+                var countryCode = itiInstance.getSelectedCountryData().iso2; // Get country code
+                console.log('Active country code (Form 1):', countryCode);
+                jQuery('.country').val(countryCode);
+            }
+        });
+
+        // For the second form
+        jQuery('#gt_phone_spc').on('keyup change', function() {
+            // Get the instance of the intlTelInput initialized on this specific input field
+            var itiInstance2 = window.intlTelInputGlobals.getInstance(jQuery('#gt_phone_spc')[0]);
+
+            if (itiInstance2) {
+                var countryCode2 = itiInstance2.getSelectedCountryData().iso2; // Get country code
+                console.log('Active country code (Form 2):', countryCode2);
+                jQuery('.countryname').val(countryCode2);
+            }
+        });
+        // ******************
+
+
+
+    });
+</script>
+<script defer>
+    var rendercaptcha = false;
+
+    function recaptchaRender() {
+        const script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.async = true;
+        script.onload = () => onloadCallback();
+        script.src = "https://www.google.com/recaptcha/api.js?render=6Lfv8FAqAAAAAPTyn2ECnErjld5Ayj_VpN9xE9KN";
+        document.head.appendChild(script);
+    }
+    if (rendercaptcha == false) {
+        setTimeout(recaptchaRender, 6000);
+    }
+    var onloadCallback = function() {
+        if (grecaptcha) {
+            rendercaptcha = true;
+            console.log('Captcha Loaded');
+        }
+    }
+</script>
+<?php wp_footer(); ?>
+</body>
+
+</html>
